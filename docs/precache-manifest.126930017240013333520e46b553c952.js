f40c3062a0fe35fae3feba2ed4bc3ab9effd2dea
kafecode <kafecode@gmail.com>
2020-08-04 14:35:35 -0400

    directorio docs para gitpages

---
+++ docs/precache-manifest.126930017240013333520e46b553c952.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591a6fcc7f0cc340279695c0425d4df5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5ecca5acb749c1d4958",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "95e1b11e69bfb9d18f91",
+    "url": "/static/css/main.9b142788.chunk.css"
+  },
+  {
+    "revision": "f5ecca5acb749c1d4958",
+    "url": "/static/js/2.35e86b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35e86b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e1b11e69bfb9d18f91",
+    "url": "/static/js/main.cdbec037.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
